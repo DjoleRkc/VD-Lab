5af1d14da59733bfffd5b4648bc0c0252cfc8336
Djole <mailzaskolu12@gmail.com>
2024-05-13 20:58:17 +0200

    Odradjena Prva Strana

---
+++ igra.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+	var field = [10][20];
+});
--- uputstvo.js
+++ uputstvo.js
@@ -1,17 +1,16 @@
-function autoResize(textarea) {
+function autoResize() {
+	var textarea = document.getElementById("tekst-uputstva");
 	textarea.style.height = "auto";
 	textarea.style.height = textarea.scrollHeight + 10 + "px";
 }
 
-window.onload = function () {
-	var textarea = document.getElementById("tekst-uputstva");
+$(document).ready(function () {
 	var tekstUputstva =
 		"In the vast expanse of the cosmos, galaxies collide and stars are born. Light dances across the universe, painting a canvas of shimmering wonders. On a small blue planet called Earth, life flourishes in diverse forms. From towering forests to sprawling cities, the world teems with activity.midst this vibrant tapestry, human endeavors take shape. Scientists probe the mysteries of quantum mechanics, while artists capture fleeting emotions on canvas. In bustling markets, traders barter goods with a timeless rhythm. Meanwhile, children play in sunlit meadows, their laughter echoing through the air.Across continents, cultures intertwine like threads in a tapestry. Music, with its universal language, unites hearts and minds. From the haunting melodies of distant lands to the rhythmic beats of urban streets, every note tells a story.";
-	textarea.value = tekstUputstva;
-	autoResize(textarea);
-};
 
-$(document).ready(function () {
+	$("#tekst-uputstva").val(tekstUputstva);
+	autoResize();
+
 	$(".tezina-btn").click(function () {
 		var isActive = $(this).attr("data-active") === "true";
 		$(this).attr("data-active", !isActive);
@@ -22,27 +21,22 @@ $(document).ready(function () {
 			$(this).removeClass("active");
 		}
 	});
-});
 
-const selectedButtons = document.querySelectorAll(
-	'input[type="radio"]:checked'
-);
-
-selectedButtons.forEach((button) => {
-	button.addEventListener("click", function () {
-		this.checked = !this.checked;
+	$("#rezultatiBtn").click(function () {
+		window.location.href = "tetris-rezultati.html";
 	});
 });
 
 function handleSelection() {
 	const selectedButtons = document.querySelectorAll(
-		'input[type="radio"]:checked'
+		'input[type="checkbox"]:checked'
 	);
 
-	const zapocniIgruBtn = document.getElementById("zapocniIgruBtn");
-
-	zapocniIgruBtn.addEventListener("click", function () {
-		window.location.href = "tetris-igra.html";
+	selectedButtons.forEach((button) => {
+		button.addEventListener("click", function () {
+			console.log(this.checked);
+			this.checked = !this.checked;
+		});
 	});
 
 	const buttonGroup = document.querySelector(".btn-group");
@@ -51,5 +45,18 @@ function handleSelection() {
 
 	if (selectedButtons.length < 2 || selectedDiff.length != 1) {
 		alert("Morate odabrati barem dve vrste blokova i tačno jednu težinu");
+	} else {
+		let selectedBlocks = [];
+		selectedButtons.forEach((button) => {
+			let imgSrc = button.id.replace("-radio", "");
+			selectedBlocks.push(imgSrc);
+			localStorage.setItem(
+				"selectedBlocks",
+				JSON.stringify(selectedBlocks)
+			);
+			localStorage.setItem("selectedDifficulty", selectedDiff[0].value);
+		});
+
+		window.location.href = "tetris-igra.html";
 	}
 }
