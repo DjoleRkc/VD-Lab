d592204854f1c59084bf3d8b50485e42566094c2
Djole <mailzaskolu12@gmail.com>
2024-05-17 00:51:17 +0200

    Dodato pomeranje u stranu i rotacija

--- igra.js
+++ igra.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	var intervall = null;
 	var blkSz = 25;
 	var brzina = tezina === "Lako" ? 1500 : tezina === "Srednje" ? 1000 : 500;
+	var touchedDown = true;
 
 	var context = canvas.getContext("2d");
 
@@ -104,6 +105,84 @@ document.addEventListener("DOMContentLoaded", function () {
 		return maxY;
 	}
 
+	function rotiraj(trenutniOblik, smer) {
+		if (smer === "SK") {
+			allY = [];
+			allX = [];
+			for (
+				let index = 0;
+				index < trenutniOblik.oblik.length - 2;
+				index++
+			) {
+				const y = trenutniOblik.oblik[index][1];
+				allY.push(y);
+			}
+
+			visina = Math.max(...allY) - Math.min(...allY);
+			let noveKoordinate = [];
+
+			for (let i = 0; i < trenutniOblik.oblik.length - 2; i++) {
+				novoY = trenutniOblik.oblik[i][0];
+				novoX = visina - trenutniOblik.oblik[i][1];
+				allX.push(novoX);
+				noveKoordinate.push([novoX, novoY]);
+			}
+
+			let maxX = Math.max(...allX);
+			if ((maxX + trenutniOblik.x) * blkSz >= canvas.width)
+				return trenutniOblik;
+
+			noveKoordinate.push(trenutniOblik.oblik[4]);
+			noveKoordinate.push(trenutniOblik.oblik[5]);
+
+			noviOblik = {
+				oblik: noveKoordinate,
+				x: trenutniOblik.x,
+				y: trenutniOblik.y,
+				boja: trenutniOblik.boja,
+			};
+
+			return noviOblik;
+		} else if (smer === "SSK") {
+			allX = [];
+			allNewX = [];
+			for (
+				let index = 0;
+				index < trenutniOblik.oblik.length - 2;
+				index++
+			) {
+				const x = trenutniOblik.oblik[index][0];
+				allX.push(x);
+			}
+
+			visina = Math.max(...allX) - Math.min(...allX);
+			let noveKoordinate = [];
+
+			for (let i = 0; i < trenutniOblik.oblik.length - 2; i++) {
+				novoX = trenutniOblik.oblik[i][1];
+				allNewX.push(novoX);
+				novoY = visina - trenutniOblik.oblik[i][0];
+				noveKoordinate.push([novoX, novoY]);
+			}
+
+			let maxX = Math.max(...allX);
+			if ((maxX + trenutniOblik.x) * blkSz >= canvas.width)
+				return trenutniOblik;
+
+			noveKoordinate.push(trenutniOblik.oblik[4]);
+			noveKoordinate.push(trenutniOblik.oblik[5]);
+
+			noviOblik = {
+				oblik: noveKoordinate,
+				x: trenutniOblik.x,
+				y: trenutniOblik.y,
+				boja: trenutniOblik.boja,
+			};
+
+			return noviOblik;
+		}
+	}
+
 	function pomeri() {
 		if (trenutniOblik != null) {
 			wipe();
@@ -122,29 +201,77 @@ document.addEventListener("DOMContentLoaded", function () {
 			) {
 				clearInterval(intervall);
 				trenutniOblik = null;
+				touchedDown = true;
+				nacrtajRandom();
+				console.log("pala");
 			}
 		}
 	}
 
+	function pomeriLevo(trenutniOblik) {
+		if ((trenutniOblik.x - 1) * blkSz >= 0) {
+			trenutniOblik.x -= 1;
+		}
+	}
+
+	function pomeriDesno(trenutniOblik) {
+		let maxX = -Infinity;
+		for (let index = 0; index < trenutniOblik.oblik.length - 2; index++) {
+			if (trenutniOblik.oblik[index][0] > maxX)
+				maxX = trenutniOblik.oblik[index][0];
+		}
+		if ((trenutniOblik.x + maxX + 1) * blkSz < canvas.width) {
+			trenutniOblik.x += 1;
+		}
+	}
+
+	document.addEventListener("keydown", function (event) {
+		if (event.key == "z") {
+			trenutniOblik = rotiraj(trenutniOblik, "SSK");
+		} else if (event.key == "ArrowUp") {
+			trenutniOblik = rotiraj(trenutniOblik, "SK");
+		} else if (event.key == "ArrowLeft") {
+			pomeriLevo(trenutniOblik);
+		} else if (event.key == "ArrowRight") {
+			pomeriDesno(trenutniOblik);
+		}
+
+		wipe();
+
+		nacrtaj(
+			trenutniOblik.oblik,
+			trenutniOblik.x,
+			trenutniOblik.y,
+			blkSz,
+			trenutniOblik.boja
+		);
+	});
+
 	function nacrtajRandom() {
-		let randomNum = Math.floor(Math.random() * odabraniOblici.length);
+		touchedDown = false;
 
-		let randomOblik = odabraniOblici[randomNum];
+		while (true) {
+			trenutniOblik = null;
+			let randomNum = Math.floor(Math.random() * odabraniOblici.length);
 
-		oblici.forEach((element) => {
-			if (element.includes(randomOblik)) {
-				trenutniOblik = {
-					oblik: element,
-					x: Math.floor(Math.random() * 8),
-					y: 0,
-					boja: element[5],
-				};
-			}
+			let randomOblik = odabraniOblici[randomNum];
 
-			if (!intervall) {
-				intervall = setInterval(pomeri, brzina);
-			}
-		});
+			oblici.forEach((element) => {
+				if (element.includes(randomOblik)) {
+					trenutniOblik = {
+						oblik: element,
+						x: Math.floor(Math.random() * 22),
+						y: 0,
+						boja: element[5],
+					};
+				}
+			});
+
+			if (trenutniOblik != null) break;
+		}
+		if (!intervall) {
+			intervall = setInterval(pomeri, brzina);
+		}
 	}
 
 	function wipe() {
@@ -153,9 +280,3 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	nacrtajRandom();
 });
-
-/*document.addEventListener("keydown", function (event) {
-			if (event.key === "ArrowLeft") {
-				console.log("Up arrow");
-			}
-		});*/
